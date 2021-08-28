d2ba3e06aaafbd7863831c416d01cc8f1e745c17
GevorgGabrielyan <gev.gabrielyan1724@gmail.com>
2021-08-28 10:12:50 +0400

    Added stocks detail component

--- src/app/core/interfaces/IStock.ts
+++ src/app/core/interfaces/IStock.ts
@@ -11,3 +11,34 @@ export interface IStock {
   share_class_figi: string;
   ticker: string;
 }
+
+export interface IStockDetail {
+  active: boolean;
+  bloomberg: string;
+  ceo: string;
+  cik: string;
+  country: string;
+  description: string;
+  employees: number;
+  exchange: string;
+  exchangeSymbol: string;
+  figi: string;
+  hq_address: string;
+  hq_country: string;
+  hq_state: string;
+  industry: string;
+  lei: string;
+  listdate: Date;
+  logo: string;
+  marketcap: number;
+  name: string;
+  phone: string;
+  sector: string;
+  sic: number;
+  similar: string[];
+  symbol: string;
+  tags: string[];
+  type: string;
+  updated: Date
+
+}
